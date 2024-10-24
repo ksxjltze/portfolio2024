1fa3f8d2c5778555223ffe7482023d0f8fe8ae32
Lee Jia Keat <leejiakeat@gmail.com>
2024-10-24 16:01:49 +0800

    blug

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Page() {
         Lee Jia Keat
       </h1>
       <p className="mb-4">
-        {`Hello there, I'm an indie game developer making games for RATHSOFT. I specialize in gameplay programming with Unity but I also often work on cloud, web, database, etc.
-        I strongly believe that anything can be done as long as you're truly willing to put in the effort and time.`}
+        {`Hello there, I'm an indie game developer making games for RATHSOFT. I specialize in gameplay programming with Unity but I also often work with cloud, web, database, etc.
+        I strongly believe that anything can be done as long as one is truly willing to put in the effort and time.`}
       </p>
       <div className="my-8">
         <BlogPosts />
