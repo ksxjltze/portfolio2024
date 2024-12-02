84a7a39125e7aef6d756640bb331b992fefb239c
Lee Jia Keat <leejiakeat@gmail.com>
2024-12-02 22:04:23 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <p className="mb-4">
         {`Hello there, I'm a Software Engineer. I like to build things, and make cool things.`}
       </p>
-      <p>I have used various technologies and programming languages across a variety of different projects, work, school and personal. I enjoy experimenting with tech, and I have a strong passion for software development.</p>
+      <p>I have experience with various technologies and programming languages across a variety of different projects, work, school and personal. I enjoy experimenting with tech, and I have a strong passion for software development.</p>
       <br></br>
       <p>
         I've also dabbled in 3D modeling with Blender, and aspire to create characters and worlds that inspire others.
