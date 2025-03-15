d0fe1d2e1d4328413921811b2f383798f47a0d4d
Lee Jia Keat <leejiakeat@gmail.com>
2025-03-15 15:32:45 +0800

    lalala

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,21 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Lee Jia Keat
-      </h1>
-      <p className="mb-4">
-        {`Hello there, I'm a Software Engineer. I like to build things, and make cool things.`}
-      </p>
-      <p>I have experience with various technologies and programming languages across a variety of different projects, work, school and personal. I enjoy experimenting with tech, and I have a strong passion for software development.</p>
+      <div className="mb-8">
+        <p>Hello there, I'm
+          <h1 className="ml-2 inline-block text-2xl font-semibold tracking-tighter">
+            Lee Jia Keat
+          </h1>, I like to build things, and make cool things.
+        </p>
+      </div>
+      <p>I enjoy experimenting with technology, and I have a strong passion for software development, game development in particular.</p>
       <br></br>
       <p>
-        I've also dabbled in 3D modeling with Blender, and aspire to create characters and worlds that inspire others.
+        Aside from being a tech bro, I also enjoy playing around with Blender, and aspire to create characters and worlds that inspire others through technical art.
       </p>
       <br></br>
       <p>
-      Recently, I've been trying to create cool visual effects in Unity, using shaders, particles, procedural generation, post-processing, etc.
+        Recently, I've been trying to create cool visual effects in Unity, using shaders, particles, procedural generation, post-processing, etc.
       </p>
       <div className="my-8">
         <BlogPosts />
