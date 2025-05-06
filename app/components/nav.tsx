8a0d3163c1fd25b41bcc19c5477f4d1b802fcf0e
Lee Jia Keat <leejiakeat@gmail.com>
2025-05-06 11:08:34 +0800

    update

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -16,8 +16,8 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
-      <div className="lg:sticky lg:top-20">
+    <aside className="-ml-[8px] -mr-[8px] mb-12 tracking-tight">
+      <div className="lg:sticky lg:top-20 nav-container">
         <nav
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
