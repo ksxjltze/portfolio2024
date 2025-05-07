aa1ced80ef3f72c8eb2397dd812a85dc54a2c938
Lee Jia Keat <leejiakeat@gmail.com>
2025-05-07 09:56:53 +0800

    aaa

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -37,10 +37,10 @@ export function BlogLink(post, useUpdatedAt) {
     className="flex flex-col space-y-1 mb-4"
     href={`/blog/${post.slug}`}>
     <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-      <p className={`${textStyle} w-[100px] tabular-nums`}>
+      <p className={`${textStyle} hover:underline hover:font-bold w-[100px] tabular-nums`}>
         {formatBlogDate(useUpdatedAtDates ? post.metadata.updatedAt : post.metadata.publishedAt)}
       </p>
-      <p className="font-bold text-foreground transition duration-500 ease-in-out tracking-tight">
+      <p className="font-bold hover:underline text-foreground transition duration-500 ease-in-out tracking-tight">
         {post.metadata.title}
       </p>
     </div>
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -9,11 +9,15 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Work</h1>
-      <p>This page is a work in progress. Please check out my old projects <a className="font-bold text-blue-500" href="https://leejiakeat.vercel.app/projects/">here.</a>
+      <p>This page is a work in progress. Please check out my old projects <a className="font-bold underline" href="https://leejiakeat.vercel.app/projects/">here.</a>
       </p>
       <br></br>
       <p>
-        Please visit <a className="font-bold text-blue-500" href="https://www.rathsoft.com/">RATHSOFT</a> to see some of the games I've worked on.
+        Visit <a className="font-bold underline" href="https://www.rathsoft.com/">RATHSOFT</a> to see some of the games I've worked on.
+      </p>
+      <br></br>
+      <p>Play our games at
+        <a className="font-bold underline" href="https://rathsoft.itch.io/"> https://rathsoft.itch.io/</a>!
       </p>
     </section>
   )
